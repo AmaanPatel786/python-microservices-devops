8a62c597a4b24e99ad8becb038526521d6f5c3c9
AmaanPatel786 <amaanpatel038@gmail.com>
2025-08-31 20:54:18 +0530

    Terraform fixes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,33 @@
-variable "region" { type = string, default = "ap-south-1" }
-variable "instance_type" { type = string, default = "t3.micro" }
-variable "key_name" { type = string, default = null }
-variable "dockerhub_username" { type = string }
-variable "backend_tag" { type = string, default = "latest" }
-variable "frontend_tag" { type = string, default = "latest" }
-variable "logger_tag" { type = string, default = "latest" }
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  type    = string
+  default = null
+}
+
+variable "dockerhub_username" {
+  type = string
+}
+
+variable "backend_tag" {
+  type    = string
+  default = "latest"
+}
+
+variable "frontend_tag" {
+  type    = string
+  default = "latest"
+}
+
+variable "logger_tag" {
+  type    = string
+  default = "latest"
+}
